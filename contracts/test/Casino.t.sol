9db7f60bcd1e6cfa7b23d9b51970966fb968e08e
Adam Wolff <awolff@gmail.com>
2022-07-03 10:17:10 -0700

    Test signing

--- contracts/test/Casino.t.sol
+++ contracts/test/Casino.t.sol
@@ -7,9 +7,10 @@ import {Casino} from "../src/Casino.sol";
 
 contract CasinoTest is Test {
     Casino sino;
-    bytes32 constant startingNonce = "this can be anything";
+    bytes32 startingNonce;
 
     function setUp() public {
+        startingNonce = keccak256("seed string");
         sino = new Casino(startingNonce);
     }
 
@@ -17,6 +18,25 @@ contract CasinoTest is Test {
         assertTrue(true);
         bytes32 nonce = sino.nonce();
         assertEq(startingNonce, nonce);
+
+        uint256 ownerPrivateKey = 0xA11CE;
+        address owner = vm.addr(ownerPrivateKey);
+        console2.log("owner");
+        console2.log(owner);
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(ownerPrivateKey, nonce);
+        console2.log(v);
+        console2.logBytes32(r);
+        console2.logBytes32(s);
+        console2.logBytes32(nonce);
+        
+
+        address signer = ecrecover(nonce, v, r, s);
+
+        assertEq(owner, signer);
+        console2.log(signer);
+
+
+
     }
 
 }
